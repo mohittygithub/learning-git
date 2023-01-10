4f20695e3b163447f1edf18c008e57dc5866bf75
mohit <smartfriendmohit@gmail.com>
2023-01-10 11:22:43 +0530

    added comment to server.js file

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello World" });
 });
+// let's see if git knows about this comment
 
 app.listen(9000, () => {
   console.log("Server is running on port 9000");
